ac7e6cb43887c3073bb56de6d525401857eb3580
Valentina Antollini <antolliniv@hotmail.com>
2021-04-14 16:12:43 +0200

    creating first node js  sever api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 const express = require("express");
+const app = express();
+
+console.log(app);
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+});
+
+app.listen(3000, () => console.log("listening on http://localhost:3000/"));
