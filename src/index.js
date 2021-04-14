849a09c0c2ff4710e270916d3f538c0622f2090b
Valentina Antollini <antolliniv@hotmail.com>
2021-04-14 16:33:14 +0200

    Create an API Endpoint to return list of posts

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
+console.log("sand");
 
-console.log(app);
+const posts = [
+  {
+    id: "1581461442206",
+    title: "This is a New Blog Post",
+    content: "This is the content! ",
+    post_image: "uploads/post-image-1581461442199.jpg",
+    added_date: "1581461442206"
+  }
+];
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World");
+app.get("/api/posts", (req, res) => {
+  res.status(200).send(posts);
 });
 
 app.listen(3000, () => console.log("listening on http://localhost:3000/"));
